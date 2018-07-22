a8361af3b35cb47d3c3e8c1d6639738bc9b293ac
dylan <dylmyl1029@gmail.com>
2018-07-22 14:57:58 -0400

    generalize network creation

--- net.py
+++ net.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 import torch
-from torch.autograd import Variable
-from torch import nn, optim
+from torch import nn
 import torch.nn.functional as F
 import torch.nn.init as init
-from torch.distributions import Categorical as Cat
 
-def initialize_params(self, state_size, action_size, n_hidden_units=128):
-    self.seed = torch.manual_seed()
-    weights = {
-        '1': init.xavier_uniform_(nn.Parameter(torch.FloatTensor(state_size, n_hidden_units))),
-        '2': init.xavier_uniform_(nn.Parameter(torch.FloatTensor(n_hidden_units, n_hidden_units))),
-        '3': init.xavier_uniform_(nn.Parameter(torch.FloatTensor(n_hidden_units, action_size)))
-    }
-    bias = {
-        '1': nn.Parameter(torch.zeros(n_hidden_units)),
-        '2': nn.Parameter(torch.zeros(n_hidden_units)),
-        '3': nn.Parameter(torch.zeros(action_size))
-    }
+
+def initialize_params(state_size, action_size, n_hidden_units=128, n_layers=3, initialize=None):
+    if initialize is None: initialize = init.xavier_uniform_()
+    layers = [n_hidden_units] * ((n_layers - 1) * 2)
+    layers.insert(0, state_size)
+    layers.append(action_size)
+    layers = [(layers[2*i], layers[2*i+1]) for i in range(len(layers)//2)]
+
+    weights, bias = {}, {}
+
+    for i, (a, b) in enumerate(layers):
+        weights[str(i)] = initialize(nn.Parameter(torch.FloatTensor(a, b)))
+        bias[str(i)] = nn.Parameter(torch.zeros(b))
+
     return weights, bias
 
 
@@ -26,17 +26,21 @@ class Actor(nn.Module):
     def __init__(self, state_size, action_size, seed, n_hidden_units=128, n_layers=3):
         super().__init__()
         self.seed = torch.manual_seed(seed)
-        self.weights, self.bias = initialize_params(state_size, action_size, n_hidden_units)
+        self.weights, self.bias = initialize_params(state_size, action_size, n_hidden_units, n_layers)
         self.parameters = nn.ParameterList([self.weights[w] for w in self.weights.keys()] +
                                            [self.bias[b] for b in self.bias.keys()])
 
-    def forward(self, state):
-        X = F.relu(torch.mm(state, self.weights['1']) + self.bias['1'])
-        X = F.relu(torch.mm(X, self.weights['2'] + self.bias['2']))
-        X = torch.mm(X, self.weights['3'] + self.bias['3'])
+    def forward(self, X):
+        X = torch.mm(self.__forward(x, 1), self.weights['3'] + self.bias['3'])
         return F.tanh(X)
+
+    def __forward(self, X, layer):
+        if layer == len(self.weights.keys()) - 1: return X
+        else:
+            X = self.__forward(F.relu(torch.mm(X, self.weights[str(layer)]) + self.bias[str(layer)]), layer+1)
 
 class Critic(nn.Module):
     def __init__(self, state_size, action_size, seed, n_hidden_units=128, n_layers=3):
         super().__init__()
         self.seed = torch.manual_seed(seed)
+        self.weights, self.bias = initialize_params()
