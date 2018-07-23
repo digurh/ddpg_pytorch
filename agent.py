cb35bca43e325bcfc1a073f6d082c08c7a47cece
dylan <dylmyl1029@gmail.com>
2018-07-23 17:25:27 -0400

    write OUNoise

--- agent.py
+++ agent.py
@@ -7,3 +7,15 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 from net import Actor, Critic
+from experience_replay import ReplayBuffer
+
+class DDPGAgent:
+    def __init__(self, state_size, action_size, seed, n_hidden_units=128, n_layers=3):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+
+        # actor network
+        self.actor_local = Actor(state_size, action_size, seed)
+        self.actor_target = Actor(state_size, action_size, seed)
+        self.actor_opt = optim.Adam(self.actor_local.parameters, )
